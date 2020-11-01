0022b890748c146ba9f29631a2660daa01ddf098
FelipeFern <felipefernandz@hotmail.com>
2020-11-01 20:16:04 -0300

    Filters in Materias y Usuarios

--- resources/views/Carreras/index.blade.php
+++ resources/views/Carreras/index.blade.php
@@ -1,37 +1,47 @@
-@extends('layouts/app')
+
+@extends('layouts.app')
+
+@section('style')
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
+@endsection
 
 @section('content')
-
-<div class="content">                
-       
-    <div class="col-12">
-        <div class="title m-b-md">
-            <h1>Carreras</h1>
-            <table class="table table-striped ">
-                <thead>
-                    <tr>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Año Inicio</th>
-                        <th scope="col">Identificador</th>
-                        <th scope="col"> Departamento Responsable </th>                       
-                        <th scope="col">Profesor Responsable</th>                         
-                    </tr>
-                </thead>
-                <tbody>
+    <div class="page-content m-5">
+        <div class="justify-content-center">
+            <div class="title m-b-md text-center">
+                <h1>Materias</h1>
+                <table id="example" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Año Inicio</th>
+                            <th scope="col">Identificador</th>
+                            <th scope="col">Departamento Responsable </th>                       
+                            <th scope="col">Profesor Responsable</th>   
+                            <th scope="col">Materias</th>                      
+                        </tr>
+                    </thead>
+                    <tbody>
                     @foreach ($carreras as $carrera)
                         <tr>
                             <th>{{$carrera->nombre}}</th>
                             <th>{{$carrera->anio_inicio}} </th>
                             <th>{{$carrera->id_str}} </th>
                             <th>{{App\Models\Departamentos::where('id', '=', $carrera->departamento_responsable)->first()->nombre}} </th>                      
-                            <th>{{App\Models\User::where('id', '=', $carrera->profesor_responsable)->first()->nombre}}</th>                            
-                        </tr>                            
-                    @endforeach                         
-                </tbody>
-            </table>
-        </div>        
+                            <th>{{App\Models\User::where('id', '=', $carrera->profesor_responsable)->first()->nombre}}</th>
+                            <th> Materias </th>                            
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+            
+        </div>
     </div>
-              
-                              
- </div>          
+@endsection
+
+
+@section('scripts')
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js" type="application/javascript"></script>
+    <script src="/js/MateriasTable.js" type="application/javascript"></script>
 @endsection
--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -1,58 +1,65 @@
-@extends('layouts/app')
+@extends('layouts.app')
+
+@section('style')
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
+@endsection
 
 @section('content')
-
-<div class="content">                
-       
-    <div class="col-12">
-        <div class="title m-b-md">
-            <h1>Usuarios</h1>
-            <table class="table table-striped ">
-                <thead>
-                    <tr>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Apellido</th>
-                        <th scope="col">LU</th>
-                        <th scope="col"> Rol </th>
-                        @if(auth()->user()->isAdmin())
-                            <th scope="col">Usuario</th>
-                            <th scope="col">Documento</th>
-                        @endif
-                        <th scope="col">Email</th>
-                        @if(auth()->user()->isAdmin())
-                            <th scope="col">Fecha de Nacimiento</th>
-                            <th scope="col">Direccion</th>                       
-                            <th scope="col">Número de telefono</th>
-                            <th scope="col">Escuela Secundaria</th>
-                        @endif
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($users as $user)
+    <div class="page-content m-5">
+        <div class="justify-content-center">
+            <div class="title m-b-md text-center">
+                <h1>Usuarios</h1>
+                <table id="example" style="width:100%">
+                    <thead>
                         <tr>
-                            <th>{{$user->nombre}}</th>
-                            <th>{{$user->apellido}} </th>
-                            <th>{{$user->legajo}} </th>
-                            <th>{{$user->rol}} </th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Apellido</th>
+                            <th scope="col">LU</th>
+                            <th scope="col"> Rol </th>
                             @if(auth()->user()->isAdmin())
-                                <th>{{$user->nombre_usuario}} </th> 
-                                <th>{{$user->tipo_documento . ' '. $user->DNI}}</th>
-                            @endif                            
-                            <th>{{$user->email}}</th>
-                            @if(auth()->user()->isAdmin())
-                                <th>{{$user->fecha_nacimiento}}</th> 
-                                <th>{{$user->direccion_calle . ' '. $user->direccion_numero}}</th>
-                                <th>{{$user->numero_telefono}}</th> 
-                                <th>{{$user->escuela_secundaria}}</th> 
+                                <th scope="col">Usuario</th>
+                                <th scope="col">Documento</th>
                             @endif
-                            
-                        </tr>                            
-                    @endforeach                         
-                </tbody>
-            </table>
-        </div>        
+                            <th scope="col">Email</th>
+                            @if(auth()->user()->isAdmin())
+                                <th scope="col">Fecha de Nacimiento</th>
+                                <th scope="col">Direccion</th>                       
+                                <th scope="col">Número de telefono</th>
+                                <th scope="col">Escuela Secundaria</th>
+                            @endif                     
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($users as $user)
+                            <tr>
+                                <th>{{$user->nombre}}</th>
+                                <th>{{$user->apellido}} </th>
+                                <th>{{$user->legajo}} </th>
+                                <th>{{$user->rol}} </th>
+                                @if(auth()->user()->isAdmin())
+                                    <th>{{$user->nombre_usuario}} </th> 
+                                    <th>{{$user->tipo_documento . ' '. $user->DNI}}</th>
+                                @endif                            
+                                <th>{{$user->email}}</th>
+                                @if(auth()->user()->isAdmin())
+                                    <th>{{$user->fecha_nacimiento}}</th> 
+                                    <th>{{$user->direccion_calle . ' '. $user->direccion_numero}}</th>
+                                    <th>{{$user->numero_telefono}}</th> 
+                                    <th>{{$user->escuela_secundaria}}</th> 
+                                @endif
+                                
+                            </tr>                            
+                        @endforeach   
+                    </tbody>
+                </table>
+            </div>
+            
+        </div>
     </div>
-              
-                              
- </div>          
+@endsection
+
+
+@section('scripts')
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js" type="application/javascript"></script>
+    <script src="/js/MateriasTable.js" type="application/javascript"></script>
 @endsection
